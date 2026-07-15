cb41eb2b8ed4a86cc764267ee739611b7a077a5f
J.Tian <foundation@utxocp.cn>
2026-07-16 02:26:26 +0800

    Create index.ts

---
+++ integrations/trade-profile/src/index.ts
@@ -0,0 +1,4 @@
+export { TradeProfileManager } from './core/TradeProfileManager';
+export { TrustScoreCalculator } from './core/TrustScoreCalculator';
+export { ComplianceChecker } from './core/ComplianceChecker';
+export * from './types';
