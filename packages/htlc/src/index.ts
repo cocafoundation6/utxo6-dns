9c1cacc58d52c30daa36fdbe154d795b5dc17c73
J.Tian <foundation@utxocp.cn>
2026-07-18 04:38:32 +0800

    Add index.ts to export HTLC modules

---
+++ packages/htlc/src/index.ts
@@ -0,0 +1,8 @@
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair)
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './types';
+export * from './HTLCContract';
+export * from './UTXOHTLC';
+export * from './EVMHTLC';
+export * from './CrossChainSwap';
