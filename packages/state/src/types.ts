901cc233ef123907d2d9ad1c8da50c6f02247753
J.Tian <foundation@utxocp.cn>
2026-07-18 03:40:05 +0800

    Add UTXO and transaction data structures
    Defines UTXO-related data structures and transaction interfaces.

---
+++ packages/state/src/types.ts
@@ -0,0 +1,75 @@
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair) 
+// SPDX-License-Identifier: Apache-2.0 
+/** 
+* UTXO State Data Structure 
+*/ 
+export interface UTXO { 
+txid: string; 
+vout: number; 
+amount: bigint; 
+scriptPubKey: string; 
+height: number; 
+confirmations: number; 
+spent: boolean; 
+spentTxid?: string; 
+spentVout?: number; 
+address?: string; 
+} 
+/** 
+* UTXO Gather 
+*/ 
+export interface UTXOSet { 
+[txid: string]: { 
+[vout: number]: UTXO; 
+}; 
+} 
+/** 
+*Transaction Input 
+*/ 
+export interface TXInput { 
+txid: string; 
+vout: number; 
+sequence: number; 
+scriptSig?: string; 
+witness?: string[]; 
+} 
+/** 
+* Transaction Output 
+*/ 
+export interface TXOutput { 
+amount: bigint; 
+scriptPubKey: string; 
+address?: string; 
+} 
+/** 
+* UTXO 交易 
+*/ 
+export interface UTXOTransaction { 
+txid: string; 
+version: number; 
+locktime: number; 
+inputs: TXInput[]; 
+outputs: TXOutput[]; 
+fee?: bigint; 
+} 
+/** 
+* Transaction Verification Result 
+*/ 
+export interface ValidationResult { 
+valid: boolean; 
+errors?: string[]; 
+warnings?: string[]; 
+} 
+/** 
+* UTXO Query filter 
+*/ 
+export interface UTXOFilter { 
+address?: string; 
+minAmount?: bigint; 
+maxAmount?: bigint; 
+minConfirmations?: number; 
+maxConfirmations?: number; 
+fromHeight?: number; 
+toHeight?: number; 
+spent?: boolean; 
+}
