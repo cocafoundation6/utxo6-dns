01baef88ec23439271a1aeb9224663b00aa64b6d
J.Tian <foundation@utxocp.cn>
2026-07-18 06:41:04 +0800

    Add index.ts to export various modules

---
+++ packages/bis-integration/src/index.ts
@@ -0,0 +1,9 @@
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair)
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './types';
+export * from './BISLedgerClient';
+export * from './MultiLateralSettlement';
+export * from './ComplianceAnchor';
+export * from './AtomicSettlement';
+export * from './CrossJurisdictionSwap';
