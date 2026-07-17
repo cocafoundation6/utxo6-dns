28c73917279e29e2265d2dfcf719919ce2107518
J.Tian <foundation@utxocp.cn>
2026-07-18 04:04:23 +0800

    Create index.ts to export modules from the package

---
+++ packages/evm/src/index.ts
@@ -0,0 +1,7 @@
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair)
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './types';
+export * from './AAL';
+export * from './UTXOSmartContract';
+export * from './EVMAdapter';
