6e250111feeaae78eed47dde78386f36c7d29bf9
J.Tian <foundation@utxocp.cn>
2026-07-18 05:51:49 +0800

    Add index.ts to export opcode modules

---
+++ packages/opcodes/src/index.ts
@@ -0,0 +1,8 @@
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair)
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './types';
+export * from './UTXOOpCodes';
+export * from './OpCodeInterpreter';
+export * from './OpCodeEngine';
+export * from './UTXOContext';
