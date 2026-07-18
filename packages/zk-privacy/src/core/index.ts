4cb550e08d84e46252596cf6be4729beedece41e
J.Tian <foundation@utxocp.cn>
2026-07-18 10:04:56 +0800

    Remove typescript comment from index.ts

---
+++ packages/zk-privacy/src/core/index.ts
@@ -0,0 +1,8 @@
+
+// Author: Monica Zhu (CoCa Foundation / UW2ICG Chair)
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './PrivacyEngine';
+export * from './CommitmentScheme';
+export * from './ProofGenerator';
+export * from './ProofVerifier';
